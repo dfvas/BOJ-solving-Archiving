042dd06b50e635c73c3ff942c5e747da9f38b2f0
dfvas <166792689+dfvas@users.noreply.github.com>
2026-04-30 12:06:08 +0900

    Add prime factorization logic in 어려운 소인수분해.cpp
    Implement prime factorization for input numbers.

--- 수학/16563/어려운 소인수분해.cpp
+++ 수학/16563/어려운 소인수분해.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
 
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int>input(N);
+	for (int i = 0; i < N; i++)
+		cin >> input[i];
+	int max = *max_element(input.begin(), input.end());
+
+	vector<int>check(max + 1);
+	for (int i = 2; i <= max; i++)
+		check[i] = i;
+
+	for (int i = 2; i * i <= max; i++)
+	{
+		if (check[i]==i)
+		{
+			for (int j = 2 * i; j <= max; j += i)
+			{
+				if (check[j]==j)
+					check[j] = i;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		int num = input[i];
+		while (num!=1)
+		{
+			cout << check[num] << " ";
+			num /= check[num];
+		}
+		cout << "\n";
+	}
+
+}
