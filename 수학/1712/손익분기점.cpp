6075393ae0bfefcac2c0beeee0dcaeb49cd4e79d
dfvas <166792689+dfvas@users.noreply.github.com>
2026-04-30 22:28:18 +0900

    Update 손익분기점.cpp

--- 수학/1712/손익분기점.cpp
+++ 수학/1712/손익분기점.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int a, b, c,ans;
+	cin >> a >> b >> c; 
+	if (b >= c)
+		cout << "-1";   
+	else {
+		ans = a / (c - b) + 1;
+		cout << ans;
+	}
+}
