b5660241227fdb1ecf0082b69ca7b896a5509c57
dfvas <166792689+dfvas@users.noreply.github.com>
2026-05-04 12:00:15 +0900

    Create 피하자.cpp

---
+++ 그리디 알고리즘/25379/피하자.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1000001];
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	long long n, i, c0 = 0, c1 = 0, cnt0 = 0, cnt1 = 0;
+
+	cin >> n;
+	for (i = 0; i < n; i++) { cin >> arr[i]; }
+
+	for (i = 0; i < n; i++) {
+		if ((arr[i] % 2)) { 
+			c1++;
+			cnt0 += c0;
+		}
+		else { 
+			c0++;
+			cnt1 += c1;
+		}
+	}
+	cout << min(cnt0, cnt1);
+
+	return 0;
+}
